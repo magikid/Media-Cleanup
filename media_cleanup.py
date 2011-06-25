8b96b7e036be6fb6f36b59f1d96702b30c4d1bce
Chris Jones <chrisj@theprofessor.jonesarama>
2011-06-24 20:12:09 -0800

    On branch master  Changes to be committed: 	modified:   media_cleanup.py 	modified:   media_cleanup.sh 	new file:   tmp/selections.xml

--- media_cleanup.py
+++ media_cleanup.py
@@ -1,13 +1,21 @@
 import urllib
-from xml.dom import minidom
+from xml.etree.ElementTree import parse
 
 language = 'en'
-api_url = 'http://www.thetvdb.com/api/'
+api_url = 'http://www.thetvdb.com/api/%s'
+search_url = 'GetSeries.php?seriesname=%s'
 apikey = 'D2B2FFFCEEDF7E83'
 seriesname = 'LOST'
 
-def series_id(seriesname):
-	url = api_url + 'GetSeries.php?seriesname=%s' % seriesname
-	dom = minidom.parse(urllib.urlopen(url))
-	series = []
-	for node in dom.getElementsByTagName('')
+feed = urllib.urlopen(api_url % search_url % seriesname)
+rss = parse(feed)
+
+series = []
+for element in rss.findall('data/series'):
+	series.append({
+		'title' : element.get('SeriesName'),
+		'id' : element.get('seriesid'),
+		'fa' : element.get('FirstAired')
+	})
+	print series
+

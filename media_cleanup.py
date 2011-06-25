23f55223568afbf83ff5fdc41e2882f26a67e206
Chris Jones <chrisj@theprofessor.jonesarama>
2011-06-24 21:21:12 -0800

    On branch master  Changes to be committed: 	modified:   media_cleanup.py

--- media_cleanup.py
+++ media_cleanup.py
@@ -11,16 +11,23 @@ seriesname = 'LOST'
 feed = 'tmp/selections.xml'
 rss = minidom.parse(feed)
 
-shows = []
+shows = {'id': [], 'name': [], 'airdate': []}
 
 for elements in rss.getElementsByTagName('Series'):
 
 		if len(elements.getElementsByTagName('seriesid')) > 0:
-			shows.append('id': elements.getElementsByTagName('seriesid')[0].firstChild.data)
+			shows['id'].append(elements.getElementsByTagName('seriesid')[0].firstChild.data)
 		if len(elements.getElementsByTagName('SeriesName')) > 0:
-			shows.append('name': elements.getElementsByTagName('SeriesName')[0].firstChild.data)
+			shows['name'].append(elements.getElementsByTagName('SeriesName')[0].firstChild.data)
 		if len(elements.getElementsByTagName('FirstAired')) > 0:
-			shows.append('airdate': elements.getElementsByTagName('FirstAired')[0].firstChild.data)
+			shows['airdate'].append(elements.getElementsByTagName('FirstAired')[0].firstChild.data)
 
-
-print shows
+lens = []
+# lens[0] is id, lens[1] is name, lens[2] is airdate
+lens.append(len(shows['id']))
+lens.append(len(shows['name']))
+lens.append(len(shows['airdate']))
+for x in range(max(lens)):
+	print "ID: %s" % shows['id'][x]
+	print "Name: %s" % shows['name'][x]
+	print "AirDate: %s" % shows['airdate'][x]
